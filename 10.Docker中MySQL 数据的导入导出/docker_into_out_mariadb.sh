9b6299f3e53a09b02803a2608520b045218b51ce
ya2022j <62124198+ya2022j@users.noreply.github.com>
2023-02-13 11:27:00 +0900

    Update docker_into_out_mariadb.sh

--- 10.Docker中MySQL 数据的导入导出/docker_into_out_mariadb.sh
+++ 10.Docker中MySQL 数据的导入导出/docker_into_out_mariadb.sh
@@ -16,11 +16,16 @@
 
 
 # 2：备份docker数据库
+# 1 进入容器，在命令行状态下导出数据表
+# 执行mysqldump的命令都是在linux的命令行状态，而不是进入MySQL
+
+mysqldump -uroot -p -B flarum > /home/flarum.sql
+# 2 然后把容器中sql文件复制到宿主机
+ sudo docker cp mariadb:/home/flarum.sql /home/flarum.sql
+
 # 由第一步的结果可知，我们的 mysql 运行在一个叫 mysql_server 的 docker 容器中。而我们要备份的数据库就在里面，叫做 test_db。mysql 的用户名密码均为root，我们将文件备份到/opt/sql_bak文件夹下。
+# Docker容器与本地文件相互拷贝
 
-# docker exec -it  mysql_server【docker容器名称/ID】 mysqldump -uroot -p123456【数据库密码】 test_db【数据库名称】 > /opt/sql_bak/test_db.sql【导出表格路径】
-
-docker exec -it mariadb  mariadb mysqldump -uroot -p123456 flarum > /home/flarum.sql
 
 # 3：导入docker数据库
 # 方法1：
@@ -38,3 +43,6 @@ docker exec -it mariadb  mariadb mysqldump -uroot -p123456 flarum > /home/flarum
 docker cp test_mm mariadb:/root/
 # 注意：这里需要将参数 -it 更换为 -i ， 否则会报错：”the input device is not a TTY”
 
+
+
+mysqldump -h127.0.0.1 -uroot -B mysql > C:\dd.sql
