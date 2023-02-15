e2b27c1fccd94f745fe92a5ef53627b1da18da23
ya2022j <62124198+ya2022j@users.noreply.github.com>
2023-02-15 17:44:15 +0900

    Update install_yarn.sh

--- 13.install_yarn/install_yarn.sh
+++ 13.install_yarn/install_yarn.sh
@@ -1,13 +1,15 @@
+
+npm install -g yarn;
+
 # 开始安装：
 # 步骤一：在 CentOS, Fedora 和 RHEL 操作系统上，你可以通过我们的 RPM 包仓库来安装 Yarn：
-# sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo;
+sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo;
 
 # # 步骤二：然后安装运行：
-#  sudo yum install yarn;
+ sudo yum install yarn;
 
 # # 步骤三：安装完成，查看版本：
-#  yarn --version
+ yarn --version
 
 
 
-npm install -g yarn
