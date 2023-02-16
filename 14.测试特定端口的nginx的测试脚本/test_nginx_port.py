e23e909cf836b5153fa281dff30ab6895926a6f3
ya2022j <62124198+ya2022j@users.noreply.github.com>
2023-02-16 21:17:57 +0900

    Update test_nginx_port.py

--- 14.测试特定端口的nginx的测试脚本/test_nginx_port.py
+++ 14.测试特定端口的nginx的测试脚本/test_nginx_port.py
@@ -20,3 +20,21 @@ if __name__ == '__main__':
     app.run(debug=True,port=4567)
 
 
+# #CentOS开放指定端口：
+# 方式一
+# 
+# 1、开启防火墙
+# systemctl start firewalld
+# 
+# 2、开放指定端口
+# firewall-cmd --zone=public --add-port=1935/tcp --permanent
+# 命令含义：
+# –zone #作用域
+# –add-port=1935/tcp#添加端口，格式为：端口/通讯协议
+# –permanent#永久生效，没有此参数重启后失效
+# 
+# 3、重启防火墙
+# firewall-cmd --reload
+# 
+# 4、查看端口号
+# netstat -ntlp//查看当前所有tcp端口·
